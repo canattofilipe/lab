17b37439da08e55b7899c50530aa418742c0f333
Wilton Filipe Canatto <wiltonfilipe.canatto-ext@ab-inbev.com>
2022-10-25 12:25:36 -0300

    save token at local storage

--- angular/gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ angular/gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -1,17 +1,34 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AutenticacaoService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  autenticar(user: string, senha: string): Observable<any> {
-    return this.httpClient.post('http://localhost:3000/user/login', {
-      userName: user,
-      password: senha,
-    });
+  autenticar(user: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        'http://localhost:3000/user/login',
+        {
+          userName: user,
+          password: senha,
+        },
+        {
+          observe: 'response',
+        }
+      )
+      .pipe(
+        tap((resp) => {
+          const authToken = resp.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvaToken(authToken);
+        })
+      );
   }
 }
