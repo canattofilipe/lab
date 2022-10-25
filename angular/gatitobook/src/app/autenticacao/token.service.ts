d674dd68c51a8006c094e2c05413d9ef0a33bd0e
Wilton Filipe Canatto <wiltonfilipe.canatto-ext@ab-inbev.com>
2022-10-25 12:06:21 -0300

    token service

---
+++ angular/gatitobook/src/app/autenticacao/token.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenService {
+  KEY = 'token';
+
+  retornaToken() {
+    return localStorage.getItem(this.KEY) ?? '';
+  }
+
+  salvaToken(token: string) {
+    localStorage.setItem(this.KEY, token);
+  }
+
+  excluirToken() {
+    localStorage.removeItem(this.KEY);
+  }
+
+  possuiToken() {
+    return !!this.retornaToken;
+  }
+}
