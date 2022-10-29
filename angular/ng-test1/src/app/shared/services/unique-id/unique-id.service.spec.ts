d1da72e1e9c4f92fe80bc47690f1f8fca5b80c1d
Wilton Filipe Canatto <wiltonfilipe.canatto-ext@ab-inbev.com>
2022-10-29 20:39:28 -0300

    elegant test

--- angular/ng-test1/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ angular/ng-test1/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,9 +1,19 @@
 import { UniqueIdService } from './unique-id.service';
 describe(UniqueIdService.name, () => {
-  it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
 
-    expect(id).toContain('app-');
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should NOT generate duplicate IDs when called multiple times`, () => {
+    const ids = new Set();
+    const service = new UniqueIdService();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+
+    expect(ids.size).toBe(50);
   });
 });
