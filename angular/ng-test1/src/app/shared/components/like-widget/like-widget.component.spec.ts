f506f7fbd9814b12d9459bfea65afcb9262232b1
Wilton Filipe Canatto <wiltonfilipe.canatto-ext@ab-inbev.com>
2022-11-03 16:23:14 -0300

    first test

--- angular/ng-test1/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ angular/ng-test1/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,15 +1,23 @@
-import { TestBed } from '@angular/core/testing';
-import { UniqueIdService } from '../../services/unique-id/unique-id.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from './../../services/unique-id/unique-id.service';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LikeWidGetComponent } from './like-widget.component';
 
 describe(LikeWidGetComponent.name, () => {
-  let component: LikeWidGetComponent = null;
+  let fixture: ComponentFixture<LikeWidGetComponent> = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LikeWidGetComponent],
+      providers: [UniqueIdService],
+      imports: [FontAwesomeModule],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(LikeWidGetComponent);
   });
 
-  it('', () => {});
+  it('Should create component', () => {
+    const instance = fixture.componentInstance;
+    expect(instance).toBeTruthy();
+  });
 });
