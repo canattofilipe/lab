22e0b7fc29e77beaba2761f585abd4a04ee12e4a
Wilton Filipe Canatto <wiltonfilipe.canatto-ext@ab-inbev.com>
2022-11-03 16:36:35 -0300

    change detection

--- angular/ng-test1/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ angular/ng-test1/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -20,4 +20,19 @@ describe(LikeWidGetComponent.name, () => {
     const instance = fixture.componentInstance;
     expect(instance).toBeTruthy();
   });
+
+  it('Should auto generate ID when id input property is missing', () => {
+    const component = fixture.componentInstance;
+    // force lifecicle start
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy();
+  });
+
+  it('Should NOT generate ID when id input property is present', () => {
+    const component = fixture.componentInstance;
+    const someId = 'someId';
+    component.id = someId;
+    fixture.detectChanges();
+    expect(component.id).toBe(someId);
+  });
 });
