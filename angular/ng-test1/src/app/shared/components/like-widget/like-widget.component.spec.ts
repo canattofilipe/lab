0930754efdf430f6a39fdfa32083319d891d4413
Wilton Filipe Canatto <wiltonfilipe.canatto-ext@ab-inbev.com>
2022-11-03 16:16:51 -0300

    test skeleton

--- angular/ng-test1/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ angular/ng-test1/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,11 +1,14 @@
+import { TestBed } from '@angular/core/testing';
 import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 import { LikeWidGetComponent } from './like-widget.component';
 
 describe(LikeWidGetComponent.name, () => {
   let component: LikeWidGetComponent = null;
 
-  beforeEach(() => {
-    component = new LikeWidGetComponent(new UniqueIdService());
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LikeWidGetComponent],
+    }).compileComponents();
   });
 
   it('', () => {});
