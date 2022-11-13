5b6aac053ba427cf98814556c12c01fbf7fbcb3d
Wilton Filipe Canatto <wiltonfilipe.canatto-ext@ab-inbev.com>
2022-11-12 22:28:17 -0300

    an special requiriment

--- angular/ng-test2/src/app/shared/components/photo-frame/photo-frame.component.ts
+++ angular/ng-test2/src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,17 +1,41 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
   styleUrls: ['./photo-frame.component.scss'],
 })
-export class PhotoFrameComponent {
+export class PhotoFrameComponent implements OnInit, OnDestroy {
   @Output() public liked: EventEmitter<void> = new EventEmitter();
   @Input() public description = '';
   @Input() public src = '';
   @Input() public likes = 0;
+  private debounceSubject: Subject<void> = new Subject();
+  private unsubiscribe: Subject<void> = new Subject();
+
+  public ngOnInit(): void {
+    this.debounceSubject
+      .asObservable()
+      .pipe(debounceTime(500))
+      .pipe(takeUntil(this.unsubiscribe))
+      .subscribe(() => this.liked.emit());
+  }
+
+  ngOnDestroy(): void {
+    this.unsubiscribe.next();
+    this.unsubiscribe.complete();
+  }
 
   public like(): void {
-    this.liked.emit();
+    this.debounceSubject.next();
   }
 }
