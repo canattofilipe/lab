eb566482d94b2252214976a020f0d800ca2eba38
Wilton Filipe Canatto <wiltonfilipe.canatto-ext@ab-inbev.com>
2022-11-19 23:18:35 -0300

    UI events

--- angular/ng-test2/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ angular/ng-test2/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -53,4 +53,20 @@ describe(LikeWidgetComponent.name, () => {
       fixture.nativeElement.querySelector('.like-widget-container');
     likeWidgetContainer.click();
   });
+
+  it(`(D) Should display number of likes when ENTER key is pressed`, (done) => {
+    fixture.detectChanges();
+    component.liked.subscribe(() => {
+      component.likes++;
+      fixture.detectChanges();
+      const counterEl: HTMLElement =
+        fixture.nativeElement.querySelector('.like-counter');
+      expect(counterEl.textContent.trim()).toBe('1');
+      done();
+    });
+    const likeWidgetContainer: HTMLElement =
+      fixture.nativeElement.querySelector('.like-widget-container');
+    const event = new KeyboardEvent('keyup', { key: 'enter' });
+    likeWidgetContainer.dispatchEvent(event);
+  });
 });
